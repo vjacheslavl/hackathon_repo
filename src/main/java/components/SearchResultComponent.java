f06b1faf709504ffc3e4ba035198a98b8d3dfc7f
alexey shnyakin <allexxmany@gmail.com>
2017-10-20 13:57:45 +0300

    Add search result platform selector method

--- src/main/java/components/SearchResultComponent.java
+++ src/main/java/components/SearchResultComponent.java
@@ -1,7 +1,10 @@
 package components;
 
 import config.webdriver.DriverBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +14,21 @@ public class SearchResultComponent {
     Logger logger = LoggerFactory.getLogger(LoginComponent.class);
     private WebDriver driver;
 
-    private final String RESULTS_PLATFORM_SELECTOR =
-        "//h2[contains(text(), 'Just Dance 2018')]/ancestor::div[@class='s-item-container']//table//h3[contains(text(), 'PlayStation 4')]";
+    private final String RESULT_ITEM_SELECTOR = "//h2[contains(text(), '%s)]/ancestor::div[@class='s-item-container']";
+    private final String RESULT_ITEM_PLATFORM_SELECTOR = "//table//h3[contains(text(), '%s')]";
+
+    @FindBy(className = "buyingPrice")
+    private WebElement purchaseItemPrice;
 
     public SearchResultComponent() {
         this.driver = DriverBase.getDriver();
         PageFactory.initElements(driver, this);
     }
 
+    public void selectProductAndPlatform(String shopItemName, String platformName) {
+        WebElement shopItemContainer = driver.findElement(By.xpath(String.format(RESULT_ITEM_SELECTOR, shopItemName)));
+        shopItemContainer.findElement(By.xpath(String.format(RESULT_ITEM_SELECTOR, platformName)));
+        shopItemContainer.click();
+    }
 
 }
