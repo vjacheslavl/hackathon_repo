2e99ad071d490c1d53d25b87a5a334dba1dd3142
nokunevs <n.okunevs@gmail.com>
2017-10-20 13:04:37 +0300

    add implicit waits

--- src/test/java/TestContext.java
+++ src/test/java/TestContext.java
@@ -3,8 +3,12 @@ import config.webdriver.DriverBase;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
 
 import static config.ApplicationProperties.ApplicationProperty.APP_URL;
+import static config.ApplicationProperties.ApplicationProperty.WAIT_TIMEOUT_SHT;
 
 public class TestContext {
 
@@ -13,6 +17,7 @@ public class TestContext {
         DriverBase.instantiateDriverObject();
         DriverBase.getDriver().manage().window().maximize();
         DriverBase.getDriver().get(ApplicationProperties.getString(APP_URL));
+        DriverBase.getDriver().manage().timeouts().implicitlyWait(ApplicationProperties.getInteger(WAIT_TIMEOUT_SHT), TimeUnit.MILLISECONDS);
     }
 
     @AfterAll
